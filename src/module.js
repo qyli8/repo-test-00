bd28eddd3042c242e7a431f8dff5a87e8a582c72
Joanna <yuyujoanna19@gmail.com>
2018-02-17 23:08:31 +1100

    program files in src

---
+++ src/module.js
@@ -0,0 +1,7 @@
+function main ( param1, param2, params ) {   
+   console.log( " hello ", params );
+   console.log( " hello ", arguments );
+}
+
+
+module.exports = main
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const main = require('./module.js')
+
+main('abc');
