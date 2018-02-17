de129b19525f92d75ed269333602e5aada6f2caa
Joanna <yuyujoanna19@gmail.com>
2018-02-17 23:27:46 +1100

    update to module.js in b-002

--- src/module.js
+++ src/module.js
@@ -1,6 +1,12 @@
 function main ( param1, param2, params ) {   
    console.log( " hello ", params );
+   console.log( " hello ", param3 );
    console.log( " hello ", arguments );
+   if (params!==undefined && typeof params === "object" && params.length > 0  ){
+       params.forEach(function(e, idx) {
+            console.log( " index: ", idx, " item : ", e);
+       });
+   }
 }
 
 
